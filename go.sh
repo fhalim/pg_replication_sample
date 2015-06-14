04805a9f97118c2a165f5b365a513d8905e33b74
Fawad Halim <fawad@fawad.net>
2015-06-13 19:03:43 -0500

    Switch to using replication slots

--- go.sh
+++ go.sh
@@ -3,6 +3,7 @@ MD=data/master
 MP=5433
 RD=data/replica
 RP=5434
+RSN=replica1
 
 REPLICATION_USER=replicator
 REPLICATION_PASSWORD=letmereplicate
@@ -15,6 +16,7 @@ start_master() {
   cat >> "${MD}/postgresql.conf" <<EOF
 port=${MP}
 wal_level = hot_standby
+max_replication_slots = 10
 max_wal_senders = 3
 checkpoint_segments = 8
 wal_keep_segments = 8
@@ -31,6 +33,9 @@ EOF
 }
 
 start_replica() {
+  # Create replication slot
+  psql -p ${MP} template1 -c "SELECT * FROM pg_create_physical_replication_slot('${RSN}');"
+
   export PGDATA=${RD}
   rm -rf ${PGDATA}
   mkdir -p ${PGDATA}
@@ -41,6 +46,7 @@ start_replica() {
   cat > "${RD}/postgresql.conf" <<EOF
 port=${RP}
 wal_level = hot_standby
+max_replication_slots = 10
 max_wal_senders = 3
 checkpoint_segments = 8
 wal_keep_segments = 8
@@ -51,6 +57,7 @@ cat >> "${RD}/recovery.conf" <<EOF
 standby_mode = 'on'
 primary_conninfo = 'host=127.0.0.1 port=${MP} user=${REPLICATION_USER} password=${REPLICATION_PASSWORD}'
 trigger_file = '/tmp/postgresql.trigger'
+primary_slot_name = '${RSN}'
 EOF
   # Start replica
   postgres&
@@ -60,3 +67,13 @@ EOF
 
 start_master
 start_replica
+
+# Write sample data to master
+psql -p ${MP} template1 -c "CREATE DATABASE fawad;"
+psql -p ${MP} fawad -c "CREATE TABLE foo (id INT, name VARCHAR(255))"
+psql -p ${MP} fawad -c "INSERT INTO foo (id, name) VALUES (1, 'jimbob')"
+# Read back sample data from replica
+psql -p ${RP} fawad -c "\d"
+psql -p ${RP} fawad -c "SELECT * FROM foo"
+
+kill `cat *.pid`
